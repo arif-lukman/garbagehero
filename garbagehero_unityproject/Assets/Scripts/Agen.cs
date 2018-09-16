740b054e5d576738fcde4c384a50fa26f79c7a2d
gilangriandales <32030356+gilangriandales@users.noreply.github.com>
2018-09-16 18:43:33 +0700

    fungsi swipe dan click
    i commit to you mastah

---
+++ garbagehero_unityproject/Assets/Scripts/Agen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Agen : MonoBehaviour {
+	public Text touchXtext;
+	public Text touchYtext;
+
+	private float durasi = 0.1f;
+	private float waktuTersisa = 0;
+
+	void Awake(){
+		touchXtext.text="xx";
+		touchYtext.text="yy";
+
+	}
+	void Update(){
+		if (Input.touchCount == 1) {
+			if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+				
+				Touch touch = Input.GetTouch (0);
+				Vector3 touchWorld = new Vector3 (touch.position.x, touch.position.y, 0);
+
+				touchXtext.text = "Touch Pos X = " + touchWorld.x;
+				touchYtext.text = "Touch Pos Y = " + touchWorld.y;
+
+				Vector2 arahJari = Camera.main.ScreenToWorldPoint (touchWorld) - transform.position;
+				float sudut = Mathf.Atan2 (arahJari.y, arahJari.x) * Mathf.Rad2Deg;
+
+				transform.rotation = Quaternion.Euler (new Vector3 (0, 0, sudut));
+			} else if(Input.GetTouch (0).phase == TouchPhase.Began){
+				waktuTersisa = 0;
+			}
+		}
+
+		if (waktuTersisa < durasi) {
+			waktuTersisa += Time.deltaTime;
+			if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended){ 
+				touchXtext.text = "tembak " ;
+				touchYtext.text = "Ayok " ;
+			}
+		} 
+	}
+}
